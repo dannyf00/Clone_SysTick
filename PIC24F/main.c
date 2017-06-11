991b88031a83df069e9c0f081bce2b1e0f5fc3de
dannyf00 <fdan00@live.com>
2017-06-10 21:10:06 -0400

    Create main.c

--- PIC24F/main.c
+++ PIC24F/main.c
@@ -6,8 +6,8 @@
 //hardware configuration
 #define LED_PORT			LATB
 #define LED_DDR				TRISB
-#define LED					(1<<0)
-#define LED_DLY				10			//delay, in ms
+#define LED				(1<<0)
+#define LED_DLY				10		//delay, in ms
 //end hardware configuration
 
 //global defines
@@ -17,19 +17,16 @@
 
 int main(void) {
 	uint32_t time0, time1;
-	uint16_t msb, lsb;
 
-	mcu_init();							//reset the mcu
-	systick_init();						//initialize systick
+	mcu_init();					//reset the mcu
+	systick_init();					//initialize systick
 	IO_OUT(LED_DDR, LED);				//led as output
-	ei();
+	ei();						//enable global interrupts
 	while (1) {
 		IO_FLP(LED_PORT, LED);			//flip the led
 
 		time0=ticks();
 		systick_delayms(LED_DLY);		//delay_ms(LED_DLY);				//waste time
 		time1=ticks() - time0;
-		lsb = time1; msb = time1>>16;
-		if (time1>10) NOP();
 	}
 }
