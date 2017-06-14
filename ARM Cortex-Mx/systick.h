d7068b2068dec3388a3e4a8eb5a72a65f3c968fd
dannyf00 <fdan00@live.com>
2017-06-14 19:41:28 -0400

    Add files via upload

--- ARM Cortex-Mx/systick.h
+++ ARM Cortex-Mx/systick.h
@@ -14,13 +14,13 @@
 void systick_init(void);
 
 //activate the isr handler
-uint32_t ticks(void);
+uint32_t ticks(void);									//109 ticks
 
 //ticks per us - arduino styled function
+#define micros()					(ticks() / cyclesPerMicrosecond())	//204 ticks
+#define millis()					(ticks() / cyclesPerMillisecond())	//309 ticks
 #define cyclesPerMillisecond()		(F_CPU / 1000ul)
 #define cyclesPerMicrosecond()		(F_CPU / 1000000ul)
-#define millis()					(ticks() / cyclesPerMicrosecond())
-#define micros()					(ticks() / cyclesPerMicrosecond())
 
 //precision delays using ticks()
 void systick_delayms(uint32_t ms);
